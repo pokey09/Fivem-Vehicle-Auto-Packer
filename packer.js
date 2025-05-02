9746737a6e4032a721bdc3d132dc3fa909a6c353
Mega-Development-DJS <parkerduthaler5@gmail.com>
2025-05-02 15:15:17 -0400

    Update packer.js

--- packer.js
+++ packer.js
@@ -118,19 +118,20 @@ function generateFxManifest(filePath) {
   const content = `fx_version "cerulean"
 game "gta5"
 
+
 files {
-  'data/handling.meta',
-  'data/vehicles.meta',
-  'data/carcols.meta',
-  'data/carvariations.meta',
-  'data/vehiclelayouts.meta',
+	'data/**/handling.meta',
+	'data/**/vehicles.meta',
+	'data/**/carcols.meta',
+	'data/**/carvariations.meta',
+	'data/**/vehiclelayouts.meta',
 }
 
-data_file 'HANDLING_FILE' 'data/handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
-data_file 'CARCOLS_FILE' 'data/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'data/**/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
 `;
   fs.writeFileSync(filePath, content);
 }
